85acee6d6f9e77c8afea51cb72b283f96eb2eb25
varun350 <114400651+varun350@users.noreply.github.com>
2023-11-21 17:06:37 +0530

    Add files via upload

---
+++ python_script_test.py
@@ -0,0 +1,33 @@
+"""
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+start = 50
+end = 100
+
+print(f"Prime numbers between {start} and {end}:")
+for num in range(start, end + 1):
+    if is_prime(num):
+        print(num)
+"""
+import datetime
+import time
+
+# Get the current time
+start_time = datetime.datetime.now()
+
+# Calculate the end time after 24 hours
+end_time = start_time + datetime.timedelta(hours=4)
+
+# Print continuously until the end time is reached
+while datetime.datetime.now() < end_time:
+    current_time = datetime.datetime.now()
+    print(f"Current time: {current_time}")
+
+    # Delay for 1 second before the next print
+    time.sleep(1)
