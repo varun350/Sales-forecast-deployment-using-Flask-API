dd4a34ef4f456bc2de29608303cb18ff630ed09a
varun350 <114400651+varun350@users.noreply.github.com>
2023-12-04 18:08:12 +0530

    Create app_entry.py

---
+++ app_entry.py
@@ -0,0 +1,8 @@
+from your_app_module_name import flask_app  # Replace with the actual name of your app module
+
+def run():
+    # Run the Flask app
+    flask_app.run(debug=True)
+
+if __name__ == '__main__':
+    run()
